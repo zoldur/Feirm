8ca1b9bc697245acf03b72f1ff9bdba3b53f88bb
Zoldur <36329578+zoldur@users.noreply.github.com>
2018-09-23 11:11:17 +0100

    Update feirm_install.sh

--- feirm_install.sh
+++ feirm_install.sh
@@ -25,11 +25,9 @@ function download_node() {
   cd $TMP_FOLDER >/dev/null 2>&1
   wget -q $COIN_TGZ
   compile_error
-  tar xvzf $COIN_ZIP >/dev/null 2>&1
-  chmod +x $COIN_DAEMON $COIN_CLI >/dev/null 2>&1
-  chown root: $COIN_DAEMON $COIN_CLI >/dev/null 2>&1
+  tar xvzf $COIN_ZIP --strip 2 >/dev/null 2>&1
   cp $COIN_DAEMON $COIN_CLI $COIN_PATH >/dev/null 2>&1
-  cd ~ >/dev/null 2>&1
+  cd - >/dev/null 2>&1
   rm -rf $TMP_FOLDER >/dev/null 2>&1
   clear
 }
